273afcdbd466654ed455ed2928dc188d5136d05a
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-03 12:30:35 +0530

    last push

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import ShadcnPratice from "@/components/ShadcnPractice";
 import Image from "next/image";
-// import ShadcnPratice from "./components/ShadcnPratice";
 
 export default function Home() {
   return (
     <>
-      {/* <ShadcnPratice /> */}
       <ShadcnPratice />
     </>
   );
